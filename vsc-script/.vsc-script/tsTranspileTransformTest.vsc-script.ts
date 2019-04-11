119da6c49681d165348aeb0d386f1ab0e191dc2d
alfnielsen <alfnielsen@gmail.com>
2019-04-11 21:49:57 +0200

    Add vsc-script for testing ts tranformer poc

---
+++ vsc-script/.vsc-script/tsTranspileTransformTest.vsc-script.ts
@@ -0,0 +1,250 @@
+import * as vsc from 'vsc-base'
+import * as ts from 'typescript'
+
+let log = '';
+
+export async function run(_path: string) {
+	vsc.showMessage('Start transformer test')
+	const compilerOptions = {
+		module: ts.ModuleKind.CommonJS,
+		target: ts.ScriptTarget.ES2015,
+		libs: ['es6']
+	}
+
+	const sourceFileTransformer = transformerArrowFunction<ts.SourceFile>();
+
+	const testSource = `	const f = (num: number) => {
+		return [num, 'string']
+	}
+`
+	let sourceFile = ts.createSourceFile(
+		'Name1',
+		testSource,
+		ts.ScriptTarget.ES2015,
+		/*setParentNodes */ true
+	);
+	const printer: ts.Printer = ts.createPrinter();
+
+	const dummyContext = getTransformationContext(compilerOptions);
+	const dummyTransformer = sourceFileTransformer(dummyContext);
+	// const updatedSourceFile = dummyTransformer(sourceFile)
+
+	const errors = [];
+	const transformResult = ts.transform(sourceFile, [sourceFileTransformer], compilerOptions)
+
+	const updatedSourceFile = transformResult.transformed[0]
+
+	const printed = printer.printFile(updatedSourceFile)
+	transformResult.dispose()
+
+	//const json = JSON.stringify(updatedSourceFile, null, 2)
+
+	log += `
+	/* Compiled ts (Including transformer)
+	
+	${updatedSourceFile.getText()}
+	
+	*/
+		`
+	log += `
+	/* printed::
+	
+	${printed}
+	
+	*/
+		`
+
+
+	const customTransformers: ts.CustomTransformers = {
+		before: [sourceFileTransformer]
+	}
+	const customTransformersNormal: ts.CustomTransformers = {
+		before: []
+	}
+
+
+	const tst = ts.transpileModule(testSource, {
+		compilerOptions,
+		transformers: customTransformers
+	})
+	const tstNormal = ts.transpileModule(testSource, {
+		compilerOptions,
+		transformers: customTransformersNormal
+	})
+
+	log += `
+
+/* Compiled js (Normal)
+
+${tstNormal.outputText}
+
+*/
+/* Compiled js (Including transformer)
+
+${tst.outputText}
+
+*/
+	`
+
+
+	vsc.appendLineToActiveDocument('// ' + log + '\n\n');
+}
+
+/**
+ * @description
+ * Create Basic Ts Transformer \
+ * You can use https://ts-ast-viewer.com/ to generate the new node or node type.
+ * @see http://vsc-base.org/#awaitResult
+ * @vscType System
+ * @oneLineEx const transformer = vsc.createTranformer(transformerCallback)
+ * @ex 
+ * @returns (callback: (node: ts.Node) => [boolean | undefined, ts.Node | undefined]) => <T extends ts.Node>() => ts.TransformerFactory<T>
+ */
+const createTransformerFactory = (callback: (node: ts.Node) => [boolean, ts.Node | undefined]) => {
+	return function TsTransformer<T extends ts.Node>(): ts.TransformerFactory<T> {
+		return (context) => {
+			const visit: ts.Visitor = (node) => {
+				log += `\n //####### ${JSON.stringify(node.getText())} \n//`
+				const [replaceNode, tranformerResult] = callback(node);
+				if (replaceNode) {
+					return tranformerResult;
+				}
+				return ts.visitEachChild(node, (child) => visit(child), context);
+			}
+			return (node) => ts.visitNode(node, visit);
+		};
+	}
+
+}
+const parsedNodeChildrenCount = (node: ts.Node): [number, ts.Node] => {
+	let count = 0;
+	let lastChild: ts.Node
+	node.forEachChild(c => {
+		count++;
+		lastChild = c
+	})
+	return [count, lastChild];
+}
+
+// In this example we test is the node is an arrow function with only one return statement: () => { return 1 }
+// If it is we rewrite it to a lambda fundion: () =>  1
+const transformerArrowFunction = createTransformerFactory((node: ts.Node) => {
+	if (!node) { // is not an arrow funcion
+		return [false, undefined]
+	}
+	if (!ts.isArrowFunction(node)) { // is not an arrow funcion
+		return [false, undefined]
+	}
+	log += ` - isArrow`
+	const [count, child] = parsedNodeChildrenCount(node.body)
+	log += ` childCount: '${count}' `
+	if (count !== 1) { // dont have one statement
+		return [false, undefined]
+	}
+	log += ` childType: '${ts.SyntaxKind[child.kind]}' `
+
+	if (!ts.isReturnStatement(child)) { // statement is not a return statement
+		return [false, undefined]
+	}
+	log += ` - Chils is Return statement`
+	const returnNode = child
+	const returnExpression = returnNode.expression
+	if (returnExpression === undefined) { // return statement is undefined
+		return [false, undefined]
+	}
+	log += ` - All good!!!`
+	//return arrow function body with the return statement's expression
+	node.body = returnExpression
+	return [true, node]
+})
+
+/**
+ * Create a dummy context!
+ * @param compilerOptions 
+ */
+const getTransformationContext = (compilerOptions: ts.CompilerOptions) => {
+	let empty = () => { };
+	let context: ts.TransformationContext = {
+		startLexicalEnvironment: empty,
+		suspendLexicalEnvironment: empty,
+		resumeLexicalEnvironment: empty,
+		endLexicalEnvironment: () => [],
+		getCompilerOptions: () => compilerOptions,
+		hoistFunctionDeclaration: empty,
+		hoistVariableDeclaration: empty,
+		readEmitHelpers: () => undefined,
+		requestEmitHelper: empty,
+		enableEmitNotification: empty,
+		enableSubstitution: empty,
+		isEmitNotificationEnabled: () => false,
+		isSubstitutionEnabled: () => false,
+		onEmitNode: empty,
+		onSubstituteNode: (hint, node) => node,
+	};
+	return context
+}
+
+
+
+
+
+
+
+// 
+ //####### "const f = (num: number) => {\n\t\treturn [num, 'string']\n\t}\n" 
+//
+ //####### "const f = (num: number) => {\n\t\treturn [num, 'string']\n\t}" 
+//
+ //####### "const f = (num: number) => {\n\t\treturn [num, 'string']\n\t}" 
+//
+ //####### "f = (num: number) => {\n\t\treturn [num, 'string']\n\t}" 
+//
+ //####### "f" 
+//
+ //####### "(num: number) => {\n\t\treturn [num, 'string']\n\t}" 
+// - isArrow childCount: '1'  childType: 'ReturnStatement'  - Chils is Return statement - All good!!!
+/* Compiled ts (Including transformer)
+
+const f = (num: number) => {
+	return [num, 'string']
+}
+
+
+*/
+
+/* printed::
+
+const f = (num: number) => [num, 'string'];
+
+
+*/
+
+ //####### "const f = (num: number) => {\n\t\treturn [num, 'string']\n\t}\n" 
+//
+ //####### "const f = (num: number) => {\n\t\treturn [num, 'string']\n\t}" 
+//
+ //####### "const f = (num: number) => {\n\t\treturn [num, 'string']\n\t}" 
+//
+ //####### "f = (num: number) => {\n\t\treturn [num, 'string']\n\t}" 
+//
+ //####### "f" 
+//
+ //####### "(num: number) => {\n\t\treturn [num, 'string']\n\t}" 
+// - isArrow childCount: '1'  childType: 'ReturnStatement'  - Chils is Return statement - All good!!!
+
+/* Compiled js (Normal)
+
+const f = (num) => {
+	 return [num, 'string'];
+};
+
+
+*/
+/* Compiled js (Including transformer)
+
+const f = (num) => [num, 'string'];
+
+
+*/
+
+
